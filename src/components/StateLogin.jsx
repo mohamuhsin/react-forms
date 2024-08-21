fc59d2752b5680dfba438c7eba0f4abe8b26fd30
mohamuhsin <muhsinohoho@outlook.com>
2024-08-21 15:55:30 +0300

    validated input upon lost focus

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -9,18 +9,40 @@ export default function Login() {
     password: "",
   });
 
-  const emailIsInvalid =
-    enteredValues.email !== "" && !enteredValues.email.includes("@");
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log(enteredValues);
 
-    setEnteredValues(""); //resetting a form in state
+    setEnteredValues({
+      email: "",
+      password: "",
+    }); //resetting a form in state
   }
 
   function handleInputChange(identifier, value) {
-    setEnteredValues((prevValues) => ({ ...prevValues, [identifier]: value }));
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
   }
 
   // function handleEmailChange(event) {
@@ -42,6 +64,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
